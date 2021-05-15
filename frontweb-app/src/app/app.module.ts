59d724119d29022b32cb0a8c55ebd0ef59b894aa
Alisson Youssf <youssfbr@gmail.com>
2021-05-15 07:59:00 -0300

    Component Client, Bootstrap / font-awesome, css

--- frontweb-app/src/app/app.module.ts
+++ frontweb-app/src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ClientsModule } from './clients/clients.module';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ClientsModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
