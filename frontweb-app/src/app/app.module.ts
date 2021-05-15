a93f5f93f139b0eb0a96ee056a47d10a336e8c86
Alisson Youssf <youssfbr@gmail.com>
2021-05-15 09:29:54 -0300

    Add Navbar

--- frontweb-app/src/app/app.module.ts
+++ frontweb-app/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+import { TemplateModule } from './template/template.module';
 import { AppComponent } from './app.component';
 
 import { ClientsModule } from './clients/clients.module';
@@ -12,8 +13,9 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FontAwesomeModule,
     ClientsModule,
-    FontAwesomeModule
+    TemplateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
